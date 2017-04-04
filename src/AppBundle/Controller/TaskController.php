e63fceb91d254d5bf3e692cd9094981b98a90109
tzepart <tzepart@gmail.com>
2017-04-05 00:03:28 +0300

    w_add_task_create_form

---
+++ src/AppBundle/Controller/TaskController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Task;
+use AppBundle\Form\TaskFormType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+ * @Route("/task", name="task")
+ */
+class TaskController extends Controller
+{
+    /**
+     * @Route("/list/", name="task.list")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return $this->render('', array('name' => $name));
+    }
+
+    /**
+     * @Route("/new/", name="task.new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $form = $this->createForm(TaskFormType::class, null, [
+            'action' => $this->generateUrl('task.create'),
+        ]);
+
+        return [
+            'form' => $form->createView()
+        ];
+    }
+
+    /**
+     * @Route("/new", name="task.create")
+     * @Method("POST")
+     * @Template("AppBundle:Task:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Task();
+
+        $form = $this->createForm(TaskFormType::class, $entity, [
+            'action' => $this->generateUrl('task.create'),
+            'method' => 'POST'
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return [
+            'form' => $form->createView()
+        ];
+    }
+}
