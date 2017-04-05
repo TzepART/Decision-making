5d4815342354af664c0e6e9908d8a1d6b1bcb9bb
Tzepkoff Artem <Artem.Tsepkov@isobar.ru>
2017-04-05 17:31:26 +0300

    w_add_user_Fixture

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tzepart
+ * Date: 05.04.17
+ * Time: 21:08
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+
+class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('user2');
+        $user->setPlainPassword('qweqwe');
+        $user->setEmail('user@mail.com');
+        $user->setEnabled(true);
+
+        $manager->persist($user);
+        $manager->flush();
+
+        $this->addReference('example_user', $user);
+    }
+
+    public function getOrder()
+    {
+        return 1;
+    }
+}
