145f5406b1bb10b6d6d04bf6283215d98ee6f2db
Tzepkoff Artem <Artem.Tsepkov@isobar.ru>
2017-04-06 12:49:07 +0300

    t_19_add_task_fixtures

---
+++ src/AppBundle/DataFixtures/ORM/LoadTaskData.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tzepart
+ * Date: 05.04.17
+ * Time: 21:08
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\Task;
+
+class LoadTaskData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $task = new Task();
+        $task->setName('example_task');
+        $task->setUser($this->getReference('example_user'));
+
+        $manager->persist($task);
+        $manager->flush();
+
+        $this->addReference('example_task', $task);
+    }
+
+    public function getOrder()
+    {
+        return 2;
+    }
+}
