00100b2118bfbbbfac4059eb81ce24f61eb6051e
tzepart <tzepart@gmail.com>
2017-04-18 01:36:42 +0300

    w_15_add_services_files

---
+++ src/AppBundle/Services/FactoryStrategies.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tzepart
+ * Date: 11.02.17
+ * Time: 18:24
+ */
+
+namespace Controllers;
+
+
+abstract class FactoryStrategies
+{
+
+    public function __construct() {
+
+    }
+
+    public static function getStrategy($strategyName) {
+        switch ($strategyName) {
+            case SevidgStrategy::STRATEGY_NAME:
+                $strategy = new SevidgStrategy();
+                break;
+
+            case MinimaxStrategy::STRATEGY_NAME:
+                $strategy = new MinimaxStrategy();
+                break;
+
+            case HurwitzStrategy::STRATEGY_NAME:
+                $strategy = new HurwitzStrategy();
+                break;
+            case BayasLaplasStrategy::STRATEGY_NAME:
+                $strategy = new BayasLaplasStrategy();
+                break;
+
+            default:
+                $strategy = null;
+        }
+
+        return $strategy;
+    }
+
+    public abstract function getOptimalSolution($matrix, $coefficient = 0);
+
+}
