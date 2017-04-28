0ad078d1a508b2824ac9b4a7f232b47094bbc6f8
Tzepkoff Artem <Artem.Tsepkov@isobar.ru>
2017-04-28 13:27:50 +0300

    w_30_add_Criteria_and_Variant_entities

--- src/AppBundle/Entity/Criteria.php
+++ src/AppBundle/Entity/Criteria.php
@@ -8,11 +8,55 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CriteriaRepository")
+ * @ORM\Table(name="criteria")
+ */
 
 class Criteria
 {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
+
+    /**
+     * @ORM\Column(type="string")
+     */
     private $name;
+
+    /**
+     * @var \AppBundle\Entity\Task
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Task")
+     * @ORM\JoinColumns({
+     * @ORM\JoinColumn(name="task_id", referencedColumnName="id")
+     * })
+     */
+    private $task;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
 
 
     /**
@@ -30,6 +74,24 @@ class Criteria
     public function setName($name)
     {
         $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return Task
+     */
+    public function getTask(): Task
+    {
+        return $this->task;
+    }
+
+    /**
+     * @param Task $task
+     * @return $this
+     */
+    public function setTask(Task $task)
+    {
+        $this->task = $task;
         return $this;
     }
 
--- src/AppBundle/Entity/Variant.php
+++ src/AppBundle/Entity/Variant.php
@@ -8,11 +8,57 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\VariantRepository")
+ * @ORM\Table(name="variant")
+ */
+
 
 class Variant
 {
 
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
     private $name;
+
+    /**
+     * @var \AppBundle\Entity\Task
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Task")
+     * @ORM\JoinColumns({
+     * @ORM\JoinColumn(name="task_id", referencedColumnName="id")
+     * })
+     */
+    private $task;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
 
 
     /**
@@ -32,5 +78,24 @@ class Variant
         $this->name = $name;
         return $this;
     }
+
+    /**
+     * @return Task
+     */
+    public function getTask(): Task
+    {
+        return $this->task;
+    }
+
+    /**
+     * @param Task $task
+     * @return $this
+     */
+    public function setTask(Task $task)
+    {
+        $this->task = $task;
+        return $this;
+    }
+
 
 }
