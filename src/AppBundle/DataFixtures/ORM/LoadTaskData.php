98a86e7e2493804092b9a95c24c77ff9c801c427
Tzepkoff Artem <Artem.Tsepkov@isobar.ru>
2017-04-28 14:29:33 +0300

    w_30_fix_fixtures

--- src/AppBundle/DataFixtures/ORM/LoadTaskData.php
+++ src/AppBundle/DataFixtures/ORM/LoadTaskData.php
@@ -8,36 +8,19 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
-
-use AppBundle\Entity\Criteria;
-use AppBundle\Entity\Variant;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Entity\Task;
-use Faker\Factory as Faker;
 
 
 class LoadTaskData extends AbstractFixture implements OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $faker = Faker::create();
         $task = new Task();
         $task->setName('example_task')
              ->setUser($this->getReference('example_user'));
-
-        foreach ($faker->words($nb = 5, $asText = false) as $index => $word) {
-            $variant = new Criteria();
-            $variant->setName($word);
-            $task->getCriteria()->add($variant);
-        }
-
-        foreach ($faker->words($nb = 5, $asText = false) as $index => $word) {
-            $variant = new Variant();
-            $variant->setName($word);
-            $task->getVariants()->add($variant);
-        }
 
         $manager->persist($task);
         $manager->flush();
