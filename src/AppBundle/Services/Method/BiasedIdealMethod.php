ac1c11b5ab8d6eec18994a0d65bf80a2102755f6
tzepart <tzepart@gmail.com>
2017-05-13 02:23:47 +0300

    w_38_BiasedIdeal_create_logic

--- src/AppBundle/Services/Method/BiasedIdealMethod.php
+++ src/AppBundle/Services/Method/BiasedIdealMethod.php
@@ -79,24 +79,71 @@ class BiasedIdealMethod extends AbstractMethod
     private function getVariantsSortByWeight(BiasedIdealModel $matrixModel)
     {
         $result = [];
-        //Цикл по критериям
+        /*
+          Создаем IdealPositive
+          Создаем IdealNegative
+          Бежим циклом по критриям
+            Выбираем столбцы по id критерия
+            Сортируем их
+            IdealPositive добалем [indexC] = max
+            IdealNegative [indexC] = min
+
+            Делаем обновленную матрицу, где значения ячеек
+            D ij = (K+j- Kij) / (K+j- K-j)
+
+            Домножаются на веса и вычислется для каждого варианта  рас-
+            стояния от объектов до идеального, используя выражение:
+            Lpi= sum((Wj*(1-D ij))) p
+            при p от 1 .. 5
+         */
+        //Создаем IdealPositive, IdealNegative
+        $idealPositive = [];
+        $idealNegative = [];
+
+        //Бежим циклом по критриям
         foreach ($matrixModel->getVectorColumnName() as $indexC => $criterionName) {
             $significance = $matrixModel->getSignificance()[$indexC];
+            //Выбираем столбцы по id критерия
             $arColumn = $matrixModel->getColumnById($indexC);
 
-            foreach ($arColumn as $indexV => $value) {
-                if(isset($result[$indexV])){
-                    $result[$indexV] += $significance*$value;
-                }else{
-                    $result[$indexV] = $significance*$value;
-                }
+            //Сортируем
+            arsort($arColumn);
+
+            //IdealPositive добалем [indexC] = max $arColumn
+            //IdealNegative [indexC] = min $arColumn
+            $idealPositive[$indexC] = array_shift($arColumn);
+            $idealNegative[$indexC] = array_pop($arColumn);
+        }
+
+        $updateMatrix = [];
+        //Делаем обновленную матрицу, где значения ячеек
+        //D ij = (K+j- Kij) / (K+j- K-j) ->
+        // -> Wj*(1-D ij)
+        $W = $matrixModel->getSignificance();
+        foreach ($matrixModel->toArray() as $varId => $row) {
+            foreach ($row as $critId => $value) {
+                $D = ($idealPositive[$critId] - $value)/($idealPositive[$critId] - $idealNegative[$critId]);
+                $updateMatrix[$varId][$critId] = $W[$critId]*(1-$D);
             }
         }
 
-        arsort($result);
+        // Вычислим расстояния от объектов до идеального,
+        // используя выражение:
+        // Lpi= sum((Wj*(1-D ij))) p
+        // при p от 1..5
+        for($p = 1; $p <= 5; $p++){
+            $arTemp = [];
+            foreach ($updateMatrix as $varId => $row) {
+                $arTemp[$varId] = array_sum($row)**$p;
+            }
+            arsort($arTemp);
+            $result[$p] = $arTemp;
+        }
 
-        //На выходе набор наихудших вариантов по критериям
-        return array_keys($result);
+        dump($result);
+        die();
+
+        return $result;
     }
 
 }
