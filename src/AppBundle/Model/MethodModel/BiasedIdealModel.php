8a3beef2c93fefa9bd315c60ff89e2663df5948f
tzepart <tzepart@gmail.com>
2017-05-13 02:30:45 +0300

    w_38_BiasedIdeal_update_default_matrix

--- src/AppBundle/Model/MethodModel/BiasedIdealModel.php
+++ src/AppBundle/Model/MethodModel/BiasedIdealModel.php
@@ -27,20 +27,17 @@ class BiasedIdealModel extends ExtendMatrixModel
      */
     public static function getDefaultModel()
     {
-        $arVariantName = ['U1','U2','U3','U4','U5','U6','U7','U8','U9'];
-        $arCriteriaName = ['Общая площадь','Площадь кухни','Близость метро','Качество дома','Цена','Этаж'];
-        $arSignificance = [0.2,0.2,0.2,0.2,0.1,0.1];
+        $arVariantName = ['МКО1','МКО2','МКО3','МКО4','МКО5','МКО6'];
+        $arCriteriaName = ['K1','K2','K3'];
+        $arSignificance = [6,6,2];
 
         $matrix = [
-            [10,2,3,2,2,4],
-            [4,7,3,4,3,1],
-            [6,8,3,5,4,2],
-            [9,2,3,2,1,3],
-            [5,1,0.1,5,2,2],
-            [3,8,0.1,2,4,1],
-            [3,5,4,2,7,7],
-            [3,6,3,4,4,1],
-            [2,5,3,1,5,7],
+            [5.0,1.0,50.0],
+            [10.0,1.5,40.0],
+            [2.0,1.5,90.0],
+            [1.0,1.0,100.0],
+            [6.0,3.0,100.0],
+            [16.0,3.5,50.0],
         ];
 
         $matrixModel = new BiasedIdealModel($matrix);
