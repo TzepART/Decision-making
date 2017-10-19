8cf0f689a18cf96034e387cbdcb1749d16aa843e
tzepart <tzepart@gmail.com>
2017-10-19 16:18:04 +0300

    w_upd_page_create_task

--- src/AppBundle/Form/TaskFormType.php
+++ src/AppBundle/Form/TaskFormType.php
@@ -7,6 +7,7 @@ use AppBundle\Form\Type\CriteriaType;
 use AppBundle\Form\Type\VariantType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,18 +20,24 @@ class TaskFormType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')
-                ->add('variants', CollectionType::class, array(
+        $builder->add('name', TextType::class, [
+                    'label' => 'Название ',
+                 ])
+                ->add('variants', CollectionType::class, [
+                    'label' => 'Варианты',
                     'entry_type' => VariantType::class,
-                    'allow_add' => true
-                ))
-                ->add('criteria', CollectionType::class, array(
+                    'allow_add' => true,
+                ])
+                ->add('criteria', CollectionType::class, [
+                    'label' => 'Критерии',
                     'entry_type' => CriteriaType::class,
-                    'allow_add' => true
-                ))
-                ->add('save', SubmitType::class, array(
-                    'attr' => array('class' => 'save'),
-                ));
+                    'allow_add' => true,
+                ])
+                ->add('save', SubmitType::class, [
+                    'attr' => [
+                        'class' => 'btn btn-primary btn-lg btn-block'
+                    ]
+                ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
